69a00196bf3d475f36ede4c0592218e6718ade68
Kristijan Cetina <kristijan.cetina@gmail.com>
2020-11-07 16:44:26 +0100

    Update Home.vue

--- paying_customer/src/views/Home.vue
+++ paying_customer/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Ovo je novi dom za našu ubercool aplikaciju"/>
   </div>
 </template>
 
