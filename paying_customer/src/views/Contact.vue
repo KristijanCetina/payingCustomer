e365534da87ad7b6f826de6b1fe63f63b41d788d
Marko Tomljanovic <markot@windowslive.com>
2020-11-29 20:32:21 +0100

    Update Home page
    -vertical blue line and #F1F1F1
      background
    -text
    -button "GET START"
    -logo jpg
    -social media buttons

--- paying_customer/src/views/Contact.vue
+++ paying_customer/src/views/Contact.vue
@@ -1,3 +1,26 @@
 <template>
-  <div class="mx-auto" style="width: 200px; padding-top: 5%">Contact page</div>
+  <div class="mx-auto" style="width: 200px; padding-top: 5%">Contact page
+
+    <a href="#" class="mx auto fa fa-facebook"></a>
+  </div>
 </template>
+
+
+<style scoped>
+.fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+</style>
--- paying_customer/src/views/Home.vue
+++ paying_customer/src/views/Home.vue
@@ -1,19 +1,57 @@
 <template>
-<div class="grid-container">
-  <div class="left"></div>
-  <div class="right"></div>
+  <div class="container-fluid">   
+    <br><br><br>
+    <div class="row">
+      <div class="col" style="font-family: 'Roboto', sans-serif; color: #ffffff">
+        <h1>Paying Customer</h1><br><br>
+        <p>The way you get started is to quit talking and begin doing. <br><br> Walt Disney</p><br><br><br><br><br>
+        <div style="margin-left: 31%">
+          <a href="#" class="mx auto fa fa-facebook"></a>&nbsp;
+        <a href="#" class="fa fa-linkedin"></a>&nbsp;
+        <a href="#" class="fa fa-instagram"></a>
+        </div>
+        </div>
+      <div class="col-sm"><img style="width: 400px; border-radius: 83px" src="@/assets/logoPC.png" alt="Logo"></div>
+      <div class="col"><h1 style="font-family: 'Roboto', sans-serif">Make your paymants easier</h1><br><br>
+      <p style="font-family: 'Roboto', sans-serif">We will ensure you get the best experience with Paying Customer</p><br>
+        <button type="button" class="btn btn-primary btn-lg position-absolute" style="margin-left: 21%; margin-top: 25%">GET START</button>
+      </div>
+      <div class="col-1"></div>       
+    </div>
 </div>
 </template>
 
-<style >
-.grid-container {
-  display: grid;
-  grid-template-columns: 0.9fr 1.1fr;
-  grid-template-rows: 1fr;
-  gap: 0px 0px;
-  grid-template-areas:
-    "left right";
+
+
+<style>
+ body {
+    height: 100%;
 }
-.left { grid-area: left; background-color: aqua; }
-.right { grid-area: right; }
+body {
+    background: linear-gradient(90deg, #384F7B 20%, #384F7B 20%, #384F7B 40%, #F1F1F1 40%, #F1F1F1 60%, #F1F1F1 60%, #F1F1F1 80%, #F1F1F1 80%);
+}
+.fa {
+  padding: 15px;
+  font-size: 20px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 50%;
+}
+.fa:hover {
+    opacity: 0.7;
+}
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+.fa-linkedin {
+  background: #007bb5;
+  color: white;
+}
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
 </style>
+ 
