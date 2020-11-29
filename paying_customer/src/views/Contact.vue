3c55dac7c83c1924b7cdc29c22cb339bc20454f1
Marko Tomljanovic <markot@windowslive.com>
2020-11-29 20:34:02 +0100

    Contact update
    -tu sam isprabavao pa vracam kak je bilo

--- paying_customer/src/views/Contact.vue
+++ paying_customer/src/views/Contact.vue
@@ -1,26 +1,3 @@
 <template>
-  <div class="mx-auto" style="width: 200px; padding-top: 5%">Contact page
-
-    <a href="#" class="mx auto fa fa-facebook"></a>
-  </div>
+  <div class="mx-auto" style="width: 200px; padding-top: 5%">Contact page</div>
 </template>
-
-
-<style scoped>
-.fa {
-  padding: 20px;
-  font-size: 30px;
-  width: 50px;
-  text-align: center;
-  text-decoration: none;
-}
-
-.fa:hover {
-    opacity: 0.7;
-}
-
-.fa-facebook {
-  background: #3B5998;
-  color: white;
-}
-</style>
