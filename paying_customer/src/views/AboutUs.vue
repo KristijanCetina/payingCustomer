645b21e0837d5c254689eea636e10e57451871ac
Marko Tomljanovic <markot@windowslive.com>
2020-12-04 23:34:53 +0100

    Create Contact page
    -complete contact page
    -add progle photos(need update)
    -vreate contactCard and iconsSM css

--- paying_customer/src/views/AboutUs.vue
+++ paying_customer/src/views/AboutUs.vue
@@ -1,19 +1,17 @@
 <template>
  <div class="container-fluid">
-<div class="row">
-    <div class="col-xs-6 col-md-6"></div>
-    <div class="col-xs-6 col-md-4"><h1 style="margin-left: 60px">Lorem Ipsum</h1></div>
-  </div>
+
   <div class="row">
-    <div class="col-xs-6 col-md-6">
-      <img style="width: 400px; margin-left: 53px; border-radius: 83px" src="@/assets/logoPC.png" alt="Logo">
+    <div class="col-xs-6 col-md-6"><br><br>
+      <img style="width: 400px; margin-left: 53px; border-radius: 83px" src="@/assets/logoPC.png" class="img-fluid" alt="Logo">
       <div style="margin-left: 27%; margin-top: 10px">
         <a href="#" class="mx auto fa fa-facebook"></a>&nbsp;
         <a href="#" class="fa fa-linkedin"></a>&nbsp;
         <a href="#" class="fa fa-instagram"></a>
         </div>
       </div>
-    <div class="col-xs-6 col-md-4 col"><br><p style="text-align: justify;">
+    <div class="col-xs-6 col-md-4 col"><br><h1 style="margin-left: 75px">Lorem Ipsum</h1><br>
+    <p style="text-align: justify;">
      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare sem rutrum vestibulum accumsan. Fusce nec condimentum nulla. Cras cursus tristique lorem vitae vehicula. Phasellus bibendum tortor eget odio maximus, vel efficitur justo malesuada. Aenean a suscipit quam, quis pellentesque dui. Sed ac vehicula magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin lobortis metus eu risus viverra, accumsan blandit turpis semper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam erat volutpat.
     </p><br><br>
     <button type="button" class="btn btn-lg position-absolute" style="margin-left: 29%; background-color:#384F7B; color:#ffffff">CHOOSE PLAN</button>
