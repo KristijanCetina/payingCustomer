a5bd1b67c7b20fb9a53e6dcbdc30a71904988775
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2020-12-11 18:43:19 +0100

    osnovni okviri stranica postavljeni

--- paying_customer/src/views/SignIn.vue
+++ paying_customer/src/views/SignIn.vue
@@ -1,3 +1,32 @@
 <template>
-  <div class="mx-auto" style="width: 200px; padding-top: 5%">Sign In page</div>
+ <div class="container-fluid">
+   <div class="blueLine"></div>
+  <div class="row">
+    <div class="col-xs-6 col-md-6"><br><br>
+      <img style="width: 400px; margin-left: 53px; border-radius: 83px" src="@/assets/logoPC.png" class="img-fluid" alt="Logo">
+      <div style="margin-left: 27%; margin-top: 10px">
+        <a href="#" class="mx auto fa fa-facebook"></a>&nbsp;
+        <a href="#" class="fa fa-linkedin"></a>&nbsp;
+        <a href="#" class="fa fa-instagram"></a>
+        </div>
+      </div>
+    <div class="col-xs-6 col-md-4 col"><br><h1 style="margin-left: 75px">SIGNUP </h1><br>
+    <p style="text-align: justify;">
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    tu će uletiti webforma za signup 
+    </p><br><br>
+    <button type="button" class="btn btn-lg position-absolute" style="margin-left: 29%; background-color:#384F7B; color:#ffffff">CHOOSE PLAN</button>
+    </div>     
+  </div> 
+</div>
 </template>
+
+
+<style scoped>
+ .btn{
+    transition: box-shadow 0.3s;
+  }
+.btn:hover{
+    box-shadow: 20px 20px 40px 0px #384F7B;
+  }
+</style>
