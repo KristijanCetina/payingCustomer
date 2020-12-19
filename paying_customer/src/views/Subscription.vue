bb0355e413ad5ab34e16e0c971599c5bf288165e
Marko Tomljanovic <markot@windowslive.com>
2020-12-19 01:17:04 +0100

    Update dashboard
    + remove unnessesery things
    + change hrew-to, which made better preformance on switching pages
    + add some text in Subscription.vue

--- paying_customer/src/views/Subscription.vue
+++ paying_customer/src/views/Subscription.vue
@@ -1,3 +1,20 @@
 <template>
-    <div></div>
+    <div><br><br><br><br>
+        <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-3">
+		</div>
+		<div class="col-md-9">Za sada stavljamo u desni grid dok ne skuzim kako funkcionira da prepozna samo od sidebara desno. Onda cemo samo jedan grid stavit.
+		</div>
+	</div><br><br><br>
+    <div class="row">
+		<div class="col-md-3">
+		</div>
+		<div class="col-md-9">PS. i ovo za sada super funkcionira. <p>Trebao bi napraviti jos gornji navibar za one ulogirane i LogOut (mogo bi u sklopu ove komponente)</p>
+		</div>
+	</div>
+    
+	
+    </div>
+    </div>
 </template>
