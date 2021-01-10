35744c2e5cbc0ff6923af04d1c2a4bd663335e0d
Marko Tomljanovic <markot@windowslive.com>
2021-01-10 12:39:08 +0100

    Add Pagination
    + change title photo & title text
    + delete some irrelevant images from assets
    + add pagination in news.vue
    + add new cards in news.json file

--- paying_customer/src/views/News_dash.vue
+++ paying_customer/src/views/News_dash.vue
@@ -1,3 +1,28 @@
 <template>
-    <div></div>
+    <div>
+		<br>
+        <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-3">
+		<authNavi/>
+		</div>
+		<div class="col-md-9">
+		</div>
+	</div><br><br><br>
+    <div class="row">
+		<div class="col-md-3">
+		</div>
+	</div>
+    </div>
+    </div>
 </template>
+
+<script>
+import authorized_navi from "@/components/authorized_navi.vue";
+
+export default {
+name: "Subscription",
+components:{
+	"authNavi" :authorized_navi},
+}
+</script>
