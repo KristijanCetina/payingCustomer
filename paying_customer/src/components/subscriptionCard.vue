08369df8c605714e9ea0291867b04f053f41e62e
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2021-02-27 11:02:33 +0100

    Get subscription plans - connection to Firebase #111
    Ostvareno upisivanje planova u Firestore!
    Sljedeći koraci:
    čitanje iz firestora
    editing planova
    korištenje podataka iz firestora za naplatu

--- paying_customer/src/components/subscriptionCard.vue
+++ paying_customer/src/components/subscriptionCard.vue
@@ -1,6 +1,6 @@
 <template>
-<div style="padding-left: 35px;" >
-    <b-card  
+  <div style="padding-left: 35px">
+    <b-card
       style="width: 200px; height: 345px"
       body-class="text-center"
       :title="suma"
@@ -8,36 +8,35 @@
       :img-alt="naziv"
       img-top
     >
-    <!-- ako budemo htjeli dodavati za nekakv bolji izgled kartice -->
-    <!-- <b-card-img src="https://placekitten.com/480/210" alt="Image" img-top></b-card-img> -->   
-      <b-card-text v-text="tekst">
-        Za sve koji žele isporbati
-      </b-card-text>
+      <!-- ako budemo htjeli dodavati za nekakv bolji izgled kartice -->
+      <!-- <b-card-img src="https://placekitten.com/480/210" alt="Image" img-top></b-card-img> -->
+      <b-card-text v-text="tekst"> Za sve koji žele isporbati </b-card-text>
 
       <b-button
-       v-on:click="submit(price)"
-       style="background-color: #384F7B; border-color: #384F7B; bottom: 20px;"
-       block
-       pill
-       bottom
-       >Choose plan</b-button>
+        v-on:click="submit(price)"
+        style="background-color: #384f7b; border-color: #384f7b; bottom: 20px"
+        block
+        pill
+        bottom
+        >Choose plan</b-button
+      >
     </b-card>
-    </div>
+  </div>
 </template>
 
 
 <script>
-import store from '@/store';
+import store from "@/store";
 
 export default {
-  props: ["suma","slika","naziv","tekst","price"],
+  props: ["suma", "slika", "naziv", "tekst", "price"],
   methods: {
-    submit(message){
+    submit(message) {
       store.subsType = message;
       this.$router.replace({ name: "Signup" });
-      console.log(store.subsType)
+      console.log(store.subsType);
     },
-  }
+  },
 };
 </script>
 
--- paying_customer/src/views/admin/Subscription_admin.vue
+++ paying_customer/src/views/admin/Subscription_admin.vue
@@ -1,23 +1,137 @@
 <template>
-    <div class="container-fluid">
-      <div class="row">
-         <adminNavi />
-        <div class="col-md-3"></div>
-        <div class="col-md-9">
-          <p>Tu bi trebalo doci pregled vrsta pretplata...</p>
-        </div>
+  <div class="container-fluid">
+    <div class="row">
+      <adminNavi />
+      <div class="col-md-3"></div>
+      <div class="col-md-9">
+        <h3>Ovdje možete stvoriti nove pretplate</h3>
+        <template>
+          <b-button @click="$bvModal.show('modal')"
+            >Stvori novu pretplatu</b-button
+          >
+
+          <b-modal id="modal">
+            <template #modal-header="{}">
+              <!-- Emulate built in modal header close button action -->
+              <h5>Unesite podatke!</h5>
+            </template>
+
+            <template #default="{}">
+              <form>
+                <div class="form-group">
+                  <input
+                    v-model="suma"
+                    type="suma"
+                    class="form-control"
+                    id="suma"
+                    aria-describedby="emailHelp"
+                    placeholder="Unesite vrijednost pretplate, npr. 300kn"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    v-model="slika"
+                    type="slika"
+                    class="form-control"
+                    id="slika"
+                    placeholder="URL slike kojom ćete predstavljati"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    v-model="naziv"
+                    type="naziv"
+                    class="form-control"
+                    id="naziv"
+                    placeholder="Naziv po kojem ćete prepoznati plan"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    v-model="tekst"
+                    type="tekst"
+                    class="form-control"
+                    id="tekst"
+                    placeholder="Kratki opis pretplate"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    v-model="price"
+                    type="price"
+                    class="form-control"
+                    id="price"
+                    placeholder="ID cijene u Stripu, npr. price_1IAe2sB4jY1Sj3hiGgaywwAH"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    v-model="id_plan"
+                    type="id_plana"
+                    class="form-control"
+                    id="id_plana"
+                    placeholder="ID plana - mora biti jedinstven i numerički"
+                  />
+                </div>
+              </form>
+            </template>
+
+            <template #modal-footer="{ cancel }">
+              <!-- Emulate built in modal footer ok and cancel button actions -->
+              <b-button size="sm" variant="success" @click.prevent="postNewSub">
+                SAVE
+              </b-button>
+              <b-button size="sm" variant="danger" @click="cancel()">
+                Cancel
+              </b-button>
+            </template>
+          </b-modal>
+        </template>
       </div>
     </div>
+  </div>
 </template>
+
 
 <script>
 import admin_navi from "@/components/admin_navi.vue";
-
+import { db } from "@/firebase";
 
 export default {
   name: "SubsriptionAdmin",
+  data: function () {
+    return {
+      suma: "",
+      slika: "",
+      naziv: "",
+      tekst: "",
+      price: "",
+      id_plan: "",
+    };
+  },
   components: {
     adminNavi: admin_navi,
-	},
+  },
+  methods: {
+    postNewSub() {
+      //trebam postaviti tu još verifikaciju polja - ili gore u body...
+      db.collection("sub_types")
+        .add({
+          suma: this.suma,
+          slika: this.slika,
+          naziv: this.naziv,
+          tekst: this.tekst,
+          price: this.price,
+          planID: this.id_plan, //zapravo koristimo za definiranje redosljeda na options page
+        })
+        .then(() => {
+          console.log("Document successfully written!");
+        })
+        .catch((error) => {
+          console.error("Error writing document: ", error);
+        });
+    },
+  },
 };
 </script>
+//koristila za modal https://bootstrap-vue.org/docs/components/modal
