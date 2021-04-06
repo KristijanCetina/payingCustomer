13da8a551b5d654959e97a90b12c55e711a329f7
Ivana Polonijo <73447189+IvanaPolonijo@users.noreply.github.com>
2021-04-06 12:01:49 +0200

    subscription kartice - look and feel #119

--- paying_customer/src/components/subscriptionCard.vue
+++ paying_customer/src/components/subscriptionCard.vue
@@ -1,38 +1,41 @@
 <template>
-  <div style="padding-left: 35px">
-    <b-card
-      style="width: 200px; height: 345px"
-      body-class="text-center"
-      :title="suma"
-      :img-src="slika"
-      :img-alt="naziv"
-      img-top
-    >
-      <!-- ako budemo htjeli dodavati za nekakv bolji izgled kartice -->
-      <!-- <b-card-img src="https://placekitten.com/480/210" alt="Image" img-top></b-card-img> -->
-      <b-card-text v-text="tekst"> </b-card-text>
-
-      <b-button
-        v-if="userIsAdmin === false"
-        v-on:click="submit(price)"
-        style="background-color: #384f7b; border-color: #384f7b; bottom: 20px"
-        block
-        pill
-        bottom
-        >Choose plan</b-button
+  <div class="mb-3">
+    <div class="d-inline-block" style="padding-left: 35px">
+      <b-card
+        style="width: 200px"
+        body-class="text-center"
+        :title="suma"
+        :img-src="slika"
+        :img-alt="naziv"
+        img-top
       >
-      <!-- otkrij kako otvoriti samo jedan modal i editirati podatke -->
-      
-        <b-button 
-        v-if="userIsAdmin === true" 
-        v-on:click="editSub(price)"
-        style="background-color: #384f7b; border-color: #384f7b; bottom: 20px"
-        block
-        pill
-        bottom
-         >Edit</b-button
+        <!-- ako budemo htjeli dodavati za nekakv bolji izgled kartice -->
+        <!-- <b-card-img src="https://placekitten.com/480/210" alt="Image" img-top></b-card-img> -->
+        <b-card-text v-text="naziv"> </b-card-text>
+        <b-card-text v-text="tekst"> </b-card-text>
+
+        <b-button
+          v-if="userIsAdmin === false"
+          v-on:click="submit(price)"
+          style="background-color: #384f7b; border-color: #384f7b; bottom: 20px"
+          block
+          pill
+          bottom
+          >Choose plan</b-button
         >
-    </b-card>
+        <!-- otkrij kako otvoriti samo jedan modal i editirati podatke -->
+
+        <b-button
+          v-if="userIsAdmin === true"
+          v-on:click="editSub(price)"
+          style="background-color: #384f7b; border-color: #384f7b; bottom: 20px"
+          block
+          pill
+          bottom
+          >Edit</b-button
+        >
+      </b-card>
+    </div>
   </div>
 </template>
 
